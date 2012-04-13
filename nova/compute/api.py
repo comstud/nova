4650723c1f40169fa29f6896d0226f2f92eada98
Chris Behrens <cbehrens@codestud.com>
2012-04-13 05:54:48 +0000

    Add the main Cells code.
    Implements blueprint nova-compute-cells
    
    DocImpact
    
    Change-Id: I1b52788ea9d7753365d175abf39bdbc22ba822fe

--- nova/compute/api.py
+++ nova/compute/api.py
@@ -59,6 +59,7 @@ LOG = logging.getLogger(__name__)
 
 FLAGS = flags.FLAGS
 flags.DECLARE('consoleauth_topic', 'nova.consoleauth')
+flags.DECLARE('cells', 'nova.cells.opts')
 
 MAX_USERDATA_SIZE = 65535
 QUOTAS = quota.QUOTAS
@@ -913,15 +914,24 @@ class API(base.Base):
                             host=src_host, cast=False,
                             reservations=downsize_reservations)
 
-            is_up = False
+            is_up = True
             bdms = self.db.block_device_mapping_get_all_by_instance(
                 context, instance["uuid"])
-            #Note(jogo): db allows for multiple compute services per host
-            try:
-                services = self.db.service_get_all_compute_by_host(
-                        context.elevated(), instance['host'])
-            except exception.ComputeHostNotFound:
-                services = []
+            services = []
+            if (not FLAGS.cells.enable or
+                    (FLAGS.cells.enable and not instance['cell_name'])):
+                # If cells is disabled or we are actually in the cell that
+                # contains this instance. Otherwise, this will fail to find
+                # the compute host.
+                try:
+                    # Note(jogo): db allows for multiple compute services
+                    # per host
+                    services = self.db.service_get_all_compute_by_host(
+                            context.elevated(), instance['host'])
+                except exception.ComputeHostNotFound:
+                    services = []
+                is_up = False
+
             for service in services:
                 if utils.service_is_up(service):
                     is_up = True
@@ -1882,6 +1892,14 @@ class API(base.Base):
                 connect_info['port'], connect_info['internal_access_path'])
 
         return {'url': connect_info['access_url']}
+
+    def get_vnc_connect_info(self, context, instance, console_type):
+        """Used in a child cell to get console info."""
+        if not instance['host']:
+            raise exception.InstanceNotReady(instance=instance)
+        connect_info = self.compute_rpcapi.get_vnc_console(context,
+                instance=instance, console_type=console_type)
+        return connect_info
 
     @wrap_check_policy
     def get_console_output(self, context, instance, tail_length=None):
