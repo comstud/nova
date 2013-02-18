2dbd7456447d951773dc24ae5ac839b978b29649
Chris Behrens <cbehrens@codestud.com>
2013-02-18 23:48:53 +0000

    MySQLdb: Adds instance_update calls.
    Adds instance_update() and instance_update_and_get_original()
    
    Implements bp/db-mysqldb-impl

--- nova/db/mysqldb/api.py
+++ nova/db/mysqldb/api.py
@@ -138,6 +138,22 @@ class API(object):
         return orig_instance.to_dict()
 
     @dbapi_method()
+    def instance_update(self, ctxt, instance_uuid, values):
+        with self.pool.get() as conn:
+            instance = models.Models.Instance.update(conn, ctxt,
+                                                     instance_uuid, values)
+        return instance.to_dict()
+
+    @dbapi_method()
+    def instance_update_and_get_original(self, ctxt, instance_uuid,
+                                         values):
+        method = models.Models.Instance.update_and_get_original
+        with self.pool.get() as conn:
+            orig_instance, instance = method(conn, ctxt, instance_uuid,
+                                             values)
+        return orig_instance.to_dict(), instance.to_dict()
+
+    @dbapi_method()
     def instance_info_cache_get(self, ctxt, instance_uuid):
         method = models.Models.InstanceInfoCache.get
         with self.pool.get() as conn:
@@ -151,7 +167,9 @@ class API(object):
         method = models.Models.InstanceInfoCache.update
         with self.pool.get() as conn:
             info_cache = method(conn, ctxt, instance_uuid, values)
-        return info_cache.to_dict()
+        if info_cache is not None:
+            info_cache = info_cache.to_dict()
+        return info_cache
 
     @dbapi_method()
     def instance_info_cache_delete(self, ctxt, instance_uuid):
